3b7be6426ea6416bbf00f555bf66235beb18b094
Raul Palavecino <raulpalav@gmail.com>
2026-01-03 22:35:30 -0300

    Subida inicial
    Servicio Base pelado

---
+++ src/HCS_5/HCS.WinService/HCS.WinService.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Linq;
+using System.ServiceProcess;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HCS.WinService
+{
+    public partial class HCSWinService : ServiceBase
+    {
+        const string SERVICE_NAME = "BNA - Host Communications Service 5.0";
+
+        public HCSWinService()
+        {
+            InitializeComponent();
+        }
+
+        protected override void OnStart(string[] args)
+        {
+            Start(args);
+        }
+
+        protected override void OnStop()
+        {
+            Stop();
+        }
+
+        public void Start(string[] args)
+        {
+            // Aquí va la lógica de inicio del servicio
+        }
+
+        public void Stop()
+        {
+            // Aquí va la lógica de detención del servicio
+        }
+
+        void HCSServiceHost_Error(object sender, EventArgs e)
+        {
+            if (!EventLog.SourceExists(SERVICE_NAME))
+            {
+                EventLog.CreateEventSource(SERVICE_NAME, "Application");
+            }
+            EventLog.WriteEntry(SERVICE_NAME, "Fallo en el servicio WCF.");
+        }
+        void HCSServiceHost_Opened(object sender, EventArgs e)
+        {
+            if (!EventLog.SourceExists(SERVICE_NAME))
+            {
+                EventLog.CreateEventSource(SERVICE_NAME, "Application");
+            }
+            EventLog.WriteEntry(SERVICE_NAME, "Los servicios WCF están en modo Abierto.");
+        }
+        void HCSServiceHost_Closed(object sender, EventArgs e)
+        {
+            if (!EventLog.SourceExists(SERVICE_NAME))
+            {
+                EventLog.CreateEventSource(SERVICE_NAME, "Application");
+            }
+            EventLog.WriteEntry(SERVICE_NAME, "Los servicios WCF están en modo Cerrado.");
+        }
+    }
+}
---
+++ src/HCS_5/HCS.WinService/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceProcess;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HCS.WinService
+{
+    internal static class Program
+    {
+        /// <summary>
+        /// Punto de entrada principal para la aplicación.
+        /// </summary>
+        static void Main()
+        {
+#if DEBUG
+            // Modo consola para debug
+            RunAsConsole();
+#else
+            // Modo servicio normal
+            ServiceBase[] ServicesToRun;
+            ServicesToRun = new ServiceBase[]
+            {
+                new HCSWinService()
+            };
+            ServiceBase.Run(ServicesToRun);
+#endif
+        }
+
+#if DEBUG
+        /// <summary>
+        /// Ejecuta el servicio en modo consola para facilitar el debug
+        /// </summary>
+        static void RunAsConsole()
+        {
+            Console.WriteLine("Ejecutando servicio en modo consola (Debug)");
+            Console.WriteLine("Presione 'Q' para detener el servicio...\n");
+
+            var service = new HCSWinService();
+            string[] args = new string[] { };
+            service.Start(args);
+
+            ConsoleKeyInfo key;
+            do
+            {
+                key = Console.ReadKey(true);
+            } while (key.KeyChar != 'q' && key.KeyChar != 'Q');
+
+            Console.WriteLine("\nDeteniendo servicio...");
+            service.Stop();
+            Console.WriteLine("Servicio detenido.");
+        }
+#endif
+    }
+}
+
+
+/*
+1- Desde la carpeta de la solución, ejecutar el comando:
+
+dotnet build HCS_5.sln --configuration Debug
+
+2- Ejecutar el programa
+.\HCS.WinService\bin\Debug\HCS.WinService.exe
+*/
